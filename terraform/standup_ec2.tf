400aa76bd8777d3d9e0ad719fa8a614e513d126b
Allen Vailliencourt <chumpmonkey@gmail.com>
2017-12-11 09:20:36 -0500

    finalizing demo playbooks

--- terraform/standup_ec2.tf
+++ terraform/standup_ec2.tf
@@ -44,6 +44,13 @@ resource "aws_security_group" "ansible-demo-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+
   # Outbound Internet
   egress {
     from_port   = 0

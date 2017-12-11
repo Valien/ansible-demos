6ece3ae06f5262e794f583e9c6125b0db8cd0bee
Allen Vailliencourt <chumpmonkey@gmail.com>
2017-12-10 20:19:19 -0500

    Adding patch files.

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+#variable "access_key" {}
+#variable "secret_key" {}
+
+variable "region" {
+    description = "AWS region to launch server"
+    default     = "us-east-2"
+}
+variable "instance_name" {
+    default = "f8f-demo-ansible-av"
+}
+# this AMI is on us-east-2 and is an Amazon Linux AMI
+variable "ami" {
+    default = "ami-c5062ba0"
+}
+variable "size" {
+    default = "t2.micro"
+}
+
+variable "count" {
+    default = 4
+}
+
+variable "key" {}
